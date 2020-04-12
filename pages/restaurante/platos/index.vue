b89ea814c90a0cd83fed6e72033775497fe1d362
Johnny Garcia <johnnygar98@hotmail.com>
2020-04-12 16:07:52 -0500

    Mas rutas

---
+++ pages/restaurante/platos/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+
+    <h1 class="mt-2 mb-3">Historial usuarios</h1>
+
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      hide-default-footer
+      class="elevation-1"
+      :loading="loading"
+      loading-text="Loading... Please wait"
+    >
+
+      <template v-slot:top>
+        <v-text-field
+          label="Buscador:"
+          class="mx-4 mt-2 pt-2"
+        ></v-text-field>
+      </template>
+
+      <template v-slot:item.actions="{ item }" >
+        <v-icon
+          small
+          class="mx-auto"
+          @click="showMore(item)"
+        >
+          mdi-eye
+        </v-icon>
+      </template>
+    
+    </v-data-table>
+
+    <v-flex
+      xs8
+      class="mt-4 ml-auto" 
+    >
+      <v-pagination 
+        :disabled="loading"
+        value="page"
+        v-model="page"
+        color="accent"
+        :length="pageCount"
+        @input="next"
+      />
+    </v-flex>
+
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    data () {
+      return {
+        page: 1,
+        lastLoad: 1,
+        pageCount: 37,
+        sortBy: 'fecha_reserva',
+        sortDesc: false,
+        loading: false,
+        headers: [
+          { text: 'Nombre', value: 'nombre' },
+          { text: 'Precio', value: 'precio', },
+          { text: 'Accion', value: 'accion' },
+          { 
+            text: 'Acciones', 
+            value: 'actions', 
+            sortable: false,
+            align: 'center',
+          },
+        ],
+        items: [],
+      }
+    },
+    asyncData({params, error}) {
+      return axios.get('http://localhost:8000/api/v1/plato/restaurante/8')
+      .then((res) => {
+        let data = res.data;
+        if (data.status < 400) {
+          return {
+            pageCount: data.meta.last_page,
+            items: data.data
+          }
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    },
+    methods: {
+      next(page) {
+        if (this.lastLoad != page) {
+          this.loading = true;
+          axios.get('http://localhost:8000/api/v1/plato/restaurante/8?page=' + page)
+          .then((res) => {
+            let data = res.data;
+            if (data.status < 400) {
+              this.items = data.data
+            }
+            this.loading = false;
+          })
+          .catch((e) => {
+            this.loading = false;
+            console.log(e);
+          });
+          this.lastLoad = page;
+        }
+      },
+      showMore(item) {
+        console.log(item);
+      }
+    }
+  }
+
+</script>
