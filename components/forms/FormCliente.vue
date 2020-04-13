94742f18723f75f2ee4a613ffe8da30b81966e46
Johnny Garcia <johnnygar98@hotmail.com>
2020-04-13 12:38:01 -0500

    Ejemplod e formulario

---
+++ components/forms/FormCliente.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-form>
+    <v-container>
+
+      <v-row>
+        <v-col
+          cols="12"
+          md=""
+        >
+
+        <h1>Formulario cliente:</h1>
+
+        </v-col>
+      </v-row>
+
+      <v-row>
+
+        <v-col
+          cols="12"
+          md="6"
+        >
+
+          <v-text-field
+            v-model="nombre"
+            :counter="20"
+            label="Nombre cliente:"
+            required
+          >
+
+          </v-text-field>
+
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="3"
+        >
+
+          <v-text-field
+            v-model="nombre"
+            :counter="20"
+            label="Telefono:"
+            required
+          >
+
+          </v-text-field>
+
+        </v-col>
+
+      </v-row>
+
+    </v-container>
+  </v-form>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: ''
+    }
+  },
+
+  methods: {
+    guardar() {
+      // GUARDAR EN EL BACKEND 
+    }
+  },
+}
+</script>
---
+++ pages/adm/usuarios/clientes/nuevo.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+
+    <form-cliente></form-cliente>
+
+  </v-container>
+</template>
+
+
+<script>
+import FormCliente from '~/components/forms/FormCliente.vue';
+
+export default {
+  components: {
+    FormCliente
+  }
+}
+</script>
