deccfa3a6aadf45f838fa8563cc3e253e162b164
Dannes Maurad <dario45678910@gmail.com>
2020-04-23 11:38:19 -0500

    agregado by DM

--- components/forms/FormMenuDia.vue
+++ components/forms/FormMenuDia.vue
@@ -25,6 +25,15 @@
           cols="12"
           md="4"
         >
+        <v-date-picker
+      v-model="fechas"
+      type="month"
+      multiple
+    ></v-date-picker>
+
+    
+    
+<!--Button-->
             </v-col>
 
 
@@ -44,6 +53,34 @@
 
           </v-text-field>
 
+
+  <v-text-field
+            v-model="precio"
+            :counter="20"
+            label="Precio:"
+            required
+            :rules="rPrecio"
+          >
+
+          </v-text-field>
+
+<br>
+        <v-btn  :disabled="!valid"
+        class="mr-4"
+        @click="validate"
+        color="accent">Registrar</v-btn>
+        
+      
+<!--Button-->
+
+  <!--Button-->
+         
+        <v-btn   color="error"
+        :disabled="!valid"
+        class="mr-4"
+        @click="validate"> Cancelar</v-btn>
+      
+        
         </v-col>
 <!---->
         <v-col
@@ -55,160 +92,8 @@
       </v-row>
 <!--Fin ==============================================-->
 
-<!--Inicio ==============================================-->
-      <v-row>
-                 <v-col
-          cols="12"
-          md="4"
-        >
-            </v-col>
 
 
-<!----->
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-
-           <v-text-field
-            v-model="precio"
-            :counter="20"
-            label="Precio:"
-            required
-            :rules="rPrecio"
-          >
-
-          </v-text-field>
-
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-        </v-col>
-
-      
-
-      </v-row>
-<!--Fin ==============================================-->
-
-<!--Inicio ==============================================-->
-      <v-row>
-                 <v-col
-          cols="12"
-          md="4"
-        >
-
-        <v-date-picker
-        v-model="dates"
-        multiple
-      ></v-date-picker>
-
-
-            </v-col>
-
-
-<!----->
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-
-<!-- <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="dates"
-        transition="scale-transition"
-        offset-y
-        min-width="290px"
-      >
-        <template v-slot:activator="{ on }">
-          <v-combobox
-            v-model="dates"
-            multiple
-            chips
-            small-chips
-            label="Multiple picker in menu"
-            prepend-icon="event"
-            readonly
-            v-on="on"
-          ></v-combobox>
-        </template>
-        <v-date-picker v-model="dates" multiple no-title scrollable>
-          <v-spacer></v-spacer>
-          <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-          <v-btn text color="primary" @click="$refs.menu.save(dates)">OK</v-btn>
-        </v-date-picker>
-      </v-menu> -->
-
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-        </v-col>
-
-      
-
-      </v-row>
-<!--Fin ==============================================-->
-
-<!--Inicio ==============================================-->
-      <v-row>
-
-       <v-col
-          cols="12"
-          md="5"
-        >
-            </v-col>
-
-
-<!----->
-        <v-col
-          cols="12"
-          md="2"
-        >
-<!--Button-->
-
-          <div class="my-2">
-
-        <v-btn  :disabled="!valid"
-        class="mr-4"
-        @click="validate"
-        color="accent">Registrar</v-btn>
-        
-      </div>
-<!--Button-->
-
-  <!--Button-->
-          <div class="my-2">
-        <v-btn   color="error"
-        :disabled="!valid"
-        class="mr-4"
-        @click="validate">Cancelar</v-btn>
-      </div>
-<!--Button-->
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="2"
-        >
-
-
-
-
-        </v-col>
-
-      
-
-      </v-row>
-<!--Fin ==============================================-->
 
 
     </v-container>
@@ -217,12 +102,13 @@
 
 
 <script>
+
+import axios from 'axios';
 export default {
   data() {
     return {
       valid:true,
-      menu:'',
-      dates:'',
+      menuDia:[], 
       descripcion: '',
       rDescripcion: [
         v => !!v || 'Descripcion es requerido.',
@@ -232,18 +118,37 @@ export default {
       rPrecio: [
         v => !!v || 'Telefono es requerido.',
         v => /^([0-9])*$/.test(v) || 'Solo pueden ser numeros.'
-      ]
+      ],
+      fechas:[]
+      
+      //fechas
+     
     }
   },
 
   methods: {
      validate () {
+       console.log(this.fechas);
       let valido = this.$refs.form.validate();
 
       if (valido) {
-        console.log('GUARDAMOS EN EL AXIOSSS!!');
+        axios.post(this.$axios.defaults.baseURL+'api/v1/menudia', {
+          id_restaurante: this.$store.state.idRestaurante,
+          menu_dia: JSON.stringify({"menu":"menu1"}), //pendiente revisar
+          fechas: JSON.stringify(this.fechas), //pendiente revisar
+          descripcion: this.descripcion,
+          precio: this.precio
+
+                })
+                .then((response)=> {
+                    console.log('objeto guardado',response)
+                })
+                .catch((error) =>{
+                    console.log(error)
+                });
       }
     },
+     
   },
 }
 </script>
